96758b7e79e7655e6b8ef8f7d203f5575046e000
Besjan <xhikab@gmail.com>
2020-07-29 00:07:31 +0200

    Add top-down camera controller

---
+++ Features/Camera/TopDownCameraController.cs
@@ -0,0 +1,41 @@
+﻿namespace Cuku.OurCity
+{
+    using UnityEngine.InputSystem;
+    using UnityEngine;
+
+    public class TopDownCameraController : MonoBehaviour
+    {
+        public InputAction MoveAction;
+
+        public float MoveSpeed = 1.0f;
+        public float MoveLerpSpeed = 0.5f;
+
+        private Transform target;
+
+        private void Awake()
+        {
+            target = GetComponent<Transform>();
+        }
+
+        private void OnEnable()
+        {
+            MoveAction.Enable();
+        }
+
+        private void OnDisable()
+        {
+            MoveAction.Disable();
+        }
+
+        private void Update()
+        {
+            var moveValue = MoveAction.ReadValue<Vector2>() * (MoveSpeed * Time.deltaTime);
+            var targetPosition = target.position;
+
+            targetPosition.x += moveValue.x;
+            targetPosition.z += moveValue.y;
+
+            target.position = Vector3.Lerp(target.position, targetPosition, MoveLerpSpeed);
+        }
+    }
+}
